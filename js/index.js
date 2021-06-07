93c3459c943777a4901e26b797ff644e721bf123
Ivan Jimenez Luque <ivanjimluqmallorca@gmail.com>
2021-06-07 13:14:05 +0200

    pintado de personajes completado

--- js/index.js
+++ js/index.js
@@ -1,1 +1,38 @@
+/* eslint-disable import/extensions */
+import { getPersonajes, mataPersonajes } from "./funciones.js";
 
+const anyadirEventos = () => {
+  const botonCargar = document.querySelector(".cargar-personajes");
+  const botonMatarFamilia = document.querySelector(".matar-familia");
+
+  botonCargar.addEventListener("click", () => {
+    cargarPersonajes();
+  });
+  botonMatarFamilia.addEventListener("click", () => {});
+};
+
+const cargarPersonajes = async () => {
+  const personajes = await getPersonajes();
+  const padrePersonajes = document.querySelector(".personajes");
+  const personajeElementoCopia = padrePersonajes
+    .querySelector(".personaje-dummy")
+    .cloneNode(true);
+  personajeElementoCopia.classList.remove("personaje-dummy");
+
+  for (const { nombre, familia, estado } of personajes) {
+    const personajeElemento = personajeElementoCopia.cloneNode(true);
+    personajeElemento.querySelector(
+      ".personajeNombre"
+    ).textContent = `${nombre} ${familia}`;
+
+    personajeElemento.querySelector(".estado").textContent = estado
+      ? "muerto"
+      : "vivo";
+
+    padrePersonajes.append(personajeElemento);
+  }
+};
+
+(() => {
+  anyadirEventos();
+})();
