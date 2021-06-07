f8b6fd8272bd3ab7419d2bed4befd9b8c137e4d5
Ivan Jimenez Luque <ivanjimluqmallorca@gmail.com>
2021-06-07 13:39:02 +0200

    mataFamilia done

--- js/index.js
+++ js/index.js
@@ -1,38 +1,62 @@
 /* eslint-disable import/extensions */
 import { getPersonajes, mataPersonajes } from "./funciones.js";
 
+let personajes = [];
+
 const anyadirEventos = () => {
   const botonCargar = document.querySelector(".cargar-personajes");
   const botonMatarFamilia = document.querySelector(".matar-familia");
 
-  botonCargar.addEventListener("click", () => {
+  botonCargar.addEventListener("click", async () => {
+    personajes = await getPersonajes();
+    borrarPersonajes();
     cargarPersonajes();
   });
-  botonMatarFamilia.addEventListener("click", () => {});
+  botonMatarFamilia.addEventListener("click", () => {
+    mataFamilia();
+  });
 };
 
-const cargarPersonajes = async () => {
-  const personajes = await getPersonajes();
+const cargarPersonajes = () => {
   const padrePersonajes = document.querySelector(".personajes");
   const personajeElementoCopia = padrePersonajes
     .querySelector(".personaje-dummy")
     .cloneNode(true);
   personajeElementoCopia.classList.remove("personaje-dummy");
 
-  for (const { nombre, familia, estado } of personajes) {
+  for (const { nombre, familia, vivo } of personajes) {
     const personajeElemento = personajeElementoCopia.cloneNode(true);
     personajeElemento.querySelector(
       ".personajeNombre"
     ).textContent = `${nombre} ${familia}`;
 
-    personajeElemento.querySelector(".estado").textContent = estado
-      ? "muerto"
-      : "vivo";
+    personajeElemento.querySelector(".estado").textContent = vivo
+      ? "vivo"
+      : "muerto";
 
     padrePersonajes.append(personajeElemento);
   }
 };
 
-(() => {
-  anyadirEventos();
-})();
+const borrarPersonajes = () => {
+  const padrePersonajes = document.querySelector(".personajes");
+  while (padrePersonajes.lastChild && padrePersonajes.childElementCount > 1) {
+    const hijoElemento = padrePersonajes.lastChild;
+    padrePersonajes.removeChild(hijoElemento);
+  }
+};
+
+const mataFamilia = async () => {
+  const familia = document.querySelector(".familia").value;
+  try {
+    personajes = await mataPersonajes(familia);
+    borrarPersonajes();
+    cargarPersonajes();
+  } catch (error) {
+    const elementoError = document.querySelector(".mensaje");
+
+    elementoError.textContent = error.message;
+  }
+};
+
+anyadirEventos();
